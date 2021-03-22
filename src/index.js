6c80f40522e6ad490c7abf08717660a9860adeb0
Ilya Mordvinkin <ilya_mordvinkin@mail.ru>
2021-03-22 17:40:33 +0400

    feat: implement max

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) { return 0 };
+	let maximum = array[0];
+  for (i = 0; i < array.length; i++) {
+    if (maximum < array[i]) {
+      maximum = array[i];
+    };
+  };
+	return maximum;
 }
 
 exports.avg = function avg (array) {
